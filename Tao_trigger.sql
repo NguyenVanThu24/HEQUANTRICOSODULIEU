69a30fd136227bee5a4ca1e9e1c5390043d2065e
NguyenVanThu24 <162033608+NguyenVanThu24@users.noreply.github.com>
2025-04-23 23:10:32 +0700

    Add files via upload

---
+++ Tao_trigger.sql
@@ -0,0 +1,50 @@
+﻿-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Trigger (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- See additional Create Trigger templates for more
+-- examples of different Trigger statements.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Nguyen Van Thu
+-- Create date: 23/04/2025
+-- Description:	Cap nhat tong thanh toan.
+-- =============================================
+CREATE TRIGGER trg_tongthanhtoan
+ON dbo.HoaDon
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Kiểm tra nếu có dòng nào sotiengiam < 0 => báo lỗi, rollback
+    IF EXISTS (
+        SELECT 1
+        FROM inserted
+        WHERE sotiengiamgia < 0
+    )
+    BEGIN
+        RAISERROR(N'So tien giam khong duoc < 0', 16, 1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END
+
+    -- Cập nhật trường tongthanhtoan = sotiengiam
+    UPDATE H
+    SET tongthanhtoan = I.sotiengiamgia
+    FROM dbo.HoaDon H
+    INNER JOIN inserted I ON H.idhoadon = I.idhoadon;
+END;
+GO
+
---
+++ Trigger.sql
@@ -0,0 +1,62 @@
+﻿CREATE TRIGGER trg_tongthanhtoan
+ON dbo.HoaDon
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Kiểm tra điều kiện để tránh lỗi logic (như số tiền giảm < 0)
+    IF EXISTS (
+        SELECT 1 FROM inserted WHERE sotiengiamgia < 0
+    )
+    BEGIN
+        RAISERROR('So tien giam khong duoc < 0', 16, 1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END
+
+    -- Cập nhật tongthanhtoan cho các bản ghi mới
+    UPDATE H
+    SET H.tongthanhtoan = I.sotiengiamgia
+    FROM dbo.HoaDon H
+    INNER JOIN inserted I ON H.idhoadon = I.idhoadon;
+END
+
+-- Thêm dữ liệu test (batch mới)
+INSERT INTO dbo.HoaDon (
+    idhoadon,
+    iddonhang,
+    idnhanvien,
+    thoigianthanhtoan,
+    phuongthucthanhtoan,
+    sotiengiamgia
+)
+VALUES (
+    'h08',
+    'd01',
+    'n01',
+    GETDATE(),
+    'maQR',
+    50000
+);
+GO
+
+INSERT INTO dbo.HoaDon (
+    idhoadon,
+    iddonhang,
+    idnhanvien,
+    thoigianthanhtoan,
+    phuongthucthanhtoan,
+    sotiengiamgia
+)
+VALUES (
+    'h10',         -- đổi ID để không trùng
+    'd01',
+    'n01',
+    GETDATE(),
+    'maQR',
+    50000
+);
+
+SELECT * FROM dbo.HoaDon WHERE idhoadon = 'h';
+-- hoặc 'h08' nếu bạn dùng lại ID cũ
